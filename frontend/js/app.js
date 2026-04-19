33f9b765cb4c75ba4c48af983d67acfd8b008ab8
Carlos-rodrito <carlosrodriguez201904834@gmail.com>
2026-04-19 17:25:52 -0600

    Conectando

---
+++ frontend/js/app.js
@@ -0,0 +1,18 @@
+async function crearEvento() {
+    const data = {
+        titulo: document.getElementById("titulo").value,
+        fecha: document.getElementById("fecha").value,
+        lugar: document.getElementById("lugar").value
+    };
+
+    const res = await fetch("https://invitaciones-backend.onrender.com/api/eventos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+    });
+
+    const evento = await res.json();
+
+    const link = `invitacion.html?id=${evento.id}`;
+    document.getElementById("link").innerText = link;
+}
---
+++ frontend/js/invitacion.js
@@ -0,0 +1,25 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+async function cargarEvento() {
+    const res = await fetch(`https://invitaciones-backend.onrender.com/api/eventos/${id}`);
+    const evento = await res.json();
+
+    document.getElementById("titulo").innerText = evento.titulo;
+    document.getElementById("fecha").innerText = evento.fecha;
+    document.getElementById("lugar").innerText = evento.lugar;
+}
+
+async function confirmar() {
+    const nombre = document.getElementById("nombre").value;
+
+    await fetch(`http://localhost:3000/api/eventos/${id}/rsvp`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ nombre })
+    });
+
+    alert("Asistencia confirmada");
+}
+
+cargarEvento();
